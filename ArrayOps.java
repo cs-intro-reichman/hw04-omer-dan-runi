f6e758fe761b51b38747e3fa1411c49f51960d03
omer-dan-runi <omer.dan01@post.runi.ac.il>
2024-01-08 14:50:23 +0200

    first try lol

--- ArrayOps.java
+++ ArrayOps.java
@@ -4,23 +4,114 @@ public class ArrayOps {
     }
     
     public static int findMissingInt (int [] array) {
+
+        boolean flag = false;
+        for (int i = 0; i < array.length; i++)
+        {
+            for (int j = 0; j < array.length; j++)
+            {
+                if (array[j] == i)
+                {
+                    flag = true;
+                }
+            }
+            if (!flag)
+            {
+                return i;
+            }
+        }
         // Write your code here:
-        return -1;
+        return 1;
     }
 
     public static int secondMaxValue(int [] array) {
         // Write your code here:
-        return 0;
+        int max, secMax;
+
+        if (array[0] > array[1])
+        {
+            max = array[0];
+            secMax = array[1];
+        }
+        else
+        {
+            secMax = array[0];
+            max = array[1];
+        }
+
+        for (int i = 2; i < array.length; i++)
+        {
+            if (array[i] > max)
+            {
+                secMax = max;
+                max = array[i];
+            }
+            else if (array[i] > secMax)
+            {
+                secMax = array[i];
+            }
+        }
+        return secMax;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
         // Write your code here:
-        return false;
+        boolean flag = false;
+        for (int i = 0; i < array1.length; i++)
+        {
+            for (int j = 0; j < array2.length; j++)
+            {
+                if (array2[j] == array1[i])
+                {
+                    flag = true;
+                }
+            }
+            if (!flag)
+            {
+                return false;
+            }
+            flag = false;
+        }
+
+        for (int j = 0; j < array2.length; j++)
+        {
+            for (int i = 0; i < array1.length; i++)
+            {
+                if (array2[i] == array1[j])
+                {
+                    flag = true;
+                }
+            }
+            if (!flag)
+            {
+                return false;
+            }
+            flag = false;
+        }
+
+        return true;
     }
 
     public static boolean isSorted(int [] array) {
         // Write your code here:
-        return false;
+        boolean increasing = true;
+        boolean decreasing = true;
+        for (int i = 0; i < array.length - 1; i++)
+        {
+            // if there is an increase
+            if (array[i] > array[i+1])
+            {
+                decreasing = false;
+            }
+
+            // if there is a decrease
+            if (array[i] > array[i+1])
+            {
+                increasing = false;
+            }
+        }
+
+        return (increasing || decreasing);
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -27,16 +27,87 @@ public class StringOps {
 
     public static String capVowelsLowRest (String string) {
         // Write your code here:
-        return "";
+        String capsVow = "";
+        for (int i = 0; i < string.length(); i++)
+        {
+            switch (string.charAt(i))
+            {
+                case 'a': capsVow = capsVow + "A"; break;
+                case 'e': capsVow = capsVow + "E"; break;
+                case 'i': capsVow = capsVow + "I"; break;
+                case 'o': capsVow = capsVow + "O"; break;
+                case 'u': capsVow = capsVow + "U"; break;
+                default: capsVow = capsVow + string.charAt(i);
+            }
+        }
+
+        return capsVow;
     }
 
     public static String camelCase (String string) {
         // Write your code here:
-        return "";
+        String camString = "";
+        boolean newWord = true;
+        int charInt;
+        char charBack;
+
+        for (int i = 0; i < string.length(); i++)
+        {
+            if (string.charAt(i) != ' ')
+            {
+
+                charInt = (int) string.charAt(0);
+
+                // Checking if the letter is Caps
+                if (charInt < 90 && charInt > 65)
+                {
+                    // Diff in the ASCII table between lower and upper
+                    charInt -= 32;
+                }
+                // Caps on new word
+                if (newWord)
+                {
+                    charInt += 32;
+                    newWord = false;
+                }
+
+                charBack = (char) charInt;
+                camString = camString + charBack;
+            }
+            else
+            {
+                newWord = true;
+            }
+        }
+
+        return camString;
     }
 
     public static int[] allIndexOf (String string, char chr) {
         // Write your code here:
-        return new int[1];
+        int indexCount = 0; 
+        String checkingString = string;
+        for (int i = 0; i < string.length(); i++)
+        {
+            if (checkingString.indexOf(chr) == i)
+            {   
+                indexCount++;
+                checkingString = checkingString.substring(i);
+            }
+        }
+
+        int [] indexes = new int[indexCount];
+        int current = 0;
+
+        for (int i = 0; i < string.length(); i++)
+        {
+            if (string.charAt(i) == chr)
+            {
+                indexes[current] = i;
+                current++;
+            }
+        }
+
+        return indexes;
     }
 }
